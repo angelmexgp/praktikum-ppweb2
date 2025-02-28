d37fb156f5b59c3afd58cc7e454024873cdb0e72
angel <angelmexgap>
2025-02-28 16:22:21 +0700

    pertemuan2

---
+++ pertemuan-2/array_buah.php
@@ -0,0 +1,24 @@
+<?php
+    $ar_buah = ["pepaya", "mangga", "pisang", "jambu"];
+    echo $ar_buah[1];
+    echo "<br/>jumlah buah" . count ($ar_buah);
+    echo "<ol>";
+    foreach ($ar_buah as $buah) {
+        echo "<li>". $buah ."</li>";
+    } echo "</ol>";
+
+    //menambahkan buah
+    $ar_buah[] = "apel";
+
+    //menghapus buah index ke 1
+    unset($ar_buah[1]);
+
+    //mengubah buah index ke 2 jd kolak
+    $ar_buah[2] = "kolak";
+
+    //menampilkan seluruh buah dengan indexnya
+    echo "<ul>";
+    foreach ($ar_buah as $buah => $V) {
+        echo "<li>buah index - " .$buah . " adalah " . $V . "</li>";
+    } echo "</ul>";
+?>
---
+++ pertemuan-2/variable_user.php
@@ -0,0 +1,11 @@
+<?php
+    //mendefinisikan variable
+    $nama = "sina";
+    $umur = 15;
+    $bb = 40;
+
+    echo "Nama saya adalah : " .$nama;
+    echo "<br/>Umur : " .$umur. "Tahun";
+    echo "<br/>Berat badan : " .$bb. "kg";
+
+?>
